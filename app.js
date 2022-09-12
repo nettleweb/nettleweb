696223c1bcf6e2f1d3afa5348bb6b51ec69987e1
Ruochen Jia <rjia@rjia.gq>
2022-09-13 00:43:01 +0100

    update home page, ui improvement, fix some bugs with service worker

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 "use strict";
 
 self.app = {
-	"id": "whitespider",
-	"version": "1.0",
-	"fileList": []
+	debug: true,
+	cacheName: "whitespider",
+	cacheVersion: "0.1.0",
+	cacheList: [],
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET",
+		"Access-Control-Allow-Credentials": "true",
+		//"Cross-Origin-Embedder-Policy": "require-corp",
+		//"Cross-Origin-Opener-Policy": "same-origin",
+		"Referrer-Policy": "origin-when-cross-origin",
+		"X-Content-Type-Options": "nosniff",
+		//"X-Frame-Options": "SAMEORIGIN"
+	}
 };
--- sw.js
+++ sw.js
@@ -1,33 +1,55 @@
 "use strict";
 
 (() => {
-// SERVICE WORKER
 
 importScripts("/uv/uv.config.js");
 importScripts("/uv/uv.sw.js");
+importScripts("/app.js");
 
 const sw = new UVServiceWorker();
-const cacheName = `${self.location.hostname}-whitespider`;
-const cacheList = [];
+const cacheName =  `${self.location.hostname}-${app.cacheName}-${app.cacheVersion}`;
 
 async function install() {
 	let cache = await caches.open(cacheName);
-	await cache.addAll(cacheList);
+	await cache.addAll(app.cacheList);
+}
+
+async function cache(request, response) {
+	try {
+		let cache = await caches.open(cacheName);
+		await cache.put(request, response.clone());
+	} catch(err) {
+		// ignore - this is usually caused by an unsupported request method
+	}
 }
 
 async function fetchRe({ request }) {
 	let response = await caches.match(request);
 	if (response == null) {
 		response = await sw.fetch({ request });
-		try {
-			let cache = await caches.open(cacheName);
-			await cache.put(request, response.clone());
-		} catch(err) {
-			// ignore - this is usually caused by an unsupported request method
-		}
+		if (response.status == 0)
+			return response;
+
+		cache(request, response);
 	}
 
-	return response;
+	return new Response(response.body, {
+		status: response.status,
+		statusText: response.statusText,
+		headers: (() => {
+			let head = new Headers(response.headers);
+			for (let e of Object.entries(app.headers))
+				head.set(e[0], e[1]);
+			return head;
+		})()
+	});
+}
+
+async function removeOldCaches() {
+	for (let k of await caches.keys()) {
+		if (k != cacheName)
+			await caches.delete(k);
+	}
 }
 
 self.addEventListener("install", (event) => {
@@ -36,6 +58,10 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
 	event.respondWith(fetchRe(event));
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(removeOldCaches());
 });
 
 })();
