dd718730f7ff53524eabfa60d236f87faa2d1337
Ruochen Jia <rjia@rjia.gq>
2022-09-18 22:21:07 +0100

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -25,7 +25,7 @@ function Coder() {
 		url = validateUrl(url);
 		
 		let str = Array.from(url).map((ch, i) => {
-			return i % 2 ? String.fromCharCode(ch.charCodeAt(0) + 0x1000) : ch;
+			return i % 2 ? String.fromCharCode(ch.charCodeAt(0) ^ 0x7c) : ch;
 		}).join("");
 
 		return encodeURIComponent(str);
@@ -39,7 +39,7 @@ function Coder() {
 		input = decodeURIComponent(input);
 
 		let str = Array.from(input).map((ch, i) => {
-			return i % 2 ? String.fromCodePoint(ch.charCodeAt(0) - 0x1000) : ch;
+			return i % 2 ? String.fromCodePoint(ch.charCodeAt(0) ^ 0x7c) : ch;
 		}).join("");
 
 		return str + (search.length ? '?' + search.join('?') : '');
