100647fcfc42b9c944d280d8c7f62bcd24a45ef9
Ruochen Jia <rjia@rjia.gq>
2022-10-15 22:14:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 self.app = {
 	debug: true,
 	cacheName: "whitespider",
-	cacheVersion: "0.4.1",
+	cacheVersion: "0.4.2",
 	cacheList: [],
 	headers: {
 		// cross orgin isolation disabled due to issues with Firebase API
