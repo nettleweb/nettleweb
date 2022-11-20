33dda525367961fcfe71cf2965e99ff2103f0617
Ruochen Jia <rjia@rjia.gq>
2022-11-20 18:16:46 +0000

    Update main.js

--- main.js
+++ main.js
@@ -53,50 +53,11 @@ function encodeText(text) {
 }
 
 /**
- * @param {RequestInfo | URL} request 
- * @returns {Promise<Response | null>}
- */
-async function fetchE(request) {
-	try {
-		return await fetch(request);
-	} catch(err) {
-		return null;
-	}
-}
-
-/**
- * @param {{readonly name: string; readonly path?: string; readonly url?: string; preview?: string;}} content 
- * @param {HTMLElement} elem 
- */
-async function loadPreview(content, elem) {
-	const name = content.name;
-	const cached = content.preview;
-	if (cached != null) {
-		elem.setAttribute("style", `background-image: url(${cached});`);
-		return;
-	}
-
-	const response = await fetchE(new Request(`./preview/${encodeURIComponent(name)}.jpg`, {
-		method: "GET",
-		mode: "same-origin"
-	}));
-
-	let url = "res/defprev.svg";
-	if (response != null && response.ok) {
-		const buf = await response.arrayBuffer();
-		url = URL.createObjectURL(new Blob([buf], { type: "image/jpeg", endings: "native" }));
-	}
-
-	content.preview = url;
-	elem.setAttribute("style", `background-image: url(${url});`);
-}
-
-/**
  * @param {{readonly name: string; readonly path?: string; readonly url?: string; readonly preview?: string}[]} contents 
  * @param {HTMLElement} container 
  * @param {boolean} noprev 
  */
-async function updateContents(contents, container, noprev = false) {
+function updateContents(contents, container, noprev = false) {
 	container.innerHTML = "";
 	for (let content of contents) {
 		// To be super safe, always escape illegal html characters in name
@@ -108,8 +69,9 @@ async function updateContents(contents, container, noprev = false) {
 		const preview = document.createElement("div");
 		preview.className = "game-preview";
 		item.appendChild(preview);
-		if (!noprev)
-			await loadPreview(content, preview);
+		if (!noprev) {
+			preview.setAttribute("style", `background-image: url("./preview/${encodeURIComponent(content.name)}.jpg");`);
+		}
 
 		const label = document.createElement("div");
 		label.className = "game-label";
@@ -232,10 +194,10 @@ function match(contents, lowerCaseInput) {
 	return r;
 }
 
-async function loadDefaultContent() {
-	await updateContents(contents.html5Games, html5GameGrid);
-	await updateContents(contents.dosGames, dosGameGrid);
-	await updateContents(contents.flashGames, flashGameGrid);
+function loadDefaultContent() {
+	updateContents(contents.html5Games, html5GameGrid);
+	updateContents(contents.dosGames, dosGameGrid);
+	updateContents(contents.flashGames, flashGameGrid);
 }
 
 TestGameDB.load().then(() => {
@@ -358,4 +320,4 @@ document.getElementById("debug-shell").onclick = () => {
 };
 
 export const status = [!1];export const locker = { lock: () => eval(`(()=>{console.log("%c\x57h\u0069t\x65S\x70i\u0064e\u0072.\x67q","background-color:#001a1a;border:3px solid #008080;border-radius:10px;color:#ffffff;display:block;font-family:Ubuntu;font-size:24px;font-stretch:normal;font-style:normal;font-weight:600;height:fit-content;margin:10px;padding:10px;position:relative;text-align:start;text-decoration:none;width:fit-content");const n=document.documentElement.outerHTML;if("W\x68\u0069t\x65S\x70\x69\u0064e\u0072"===document.title&&n.includes("r\u0075ochenj\x69a")&&n.includes("\x77\u0068\x69t\x65\x73\u0070id\u0065r.\u0067q")){console.log("%cPage Verified", 'position: relative;display: block;width: fit-content;height: fit-content;color: #ffffff;background-color: #008000;font-size: 14px;font-weight: 600;font-family: "Ubuntu Mono";font-stretch: normal;text-align: start;text-decoration: none;');return !0;}window["_$$0Oc"]();return !1;})();`) };
-locker.lock()&&(await loadDefaultContent(),status[0]=!0);document.getElementById("background-screen").style.display="none";document.getElementById("home-screen").style.display="block";
+locker.lock()&&(loadDefaultContent(),status[0]=!0);document.getElementById("background-screen").style.display="none";document.getElementById("home-screen").style.display="block";
