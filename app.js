a5d3963edab0a3fcb418e66525ddd4094127dc3d
Ruochen Jia <rjia@rjia.gq>
2022-12-04 01:07:07 +0000

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 self.app = {
 	debug: true,
 	cacheName: "whitespider",
-	cacheVersion: "2.0.0-beta1",
+	cacheVersion: "2.0.0-beta2",
 	cacheList: [],
 	headers: {
 		"Referrer-Policy": "no-referrer",
